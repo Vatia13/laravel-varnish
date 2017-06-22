8e9292b6a769df181d07480e0f134ff1d242ad42
Salikh Gurgenidze <vatia0@gmail.com>
2017-06-22 14:56:13 +0400

    administrative_host added

--- src/Varnish.php
+++ src/Varnish.php
@@ -51,7 +51,7 @@ class Varnish
 
         $config = config('laravel-varnish');
 
-        return "sudo varnishadm -S {$config['administrative_secret']} -T 127.0.0.1:{$config['administrative_port']} 'ban req.http.host ~ {$hostsRegex}'";
+        return "sudo varnishadm -S {$config['administrative_secret']} -T {$config['administrative_host']}:{$config['administrative_port']} 'ban req.http.host ~ {$hostsRegex}'";
     }
 
     protected function executeCommand(string $command): Process
