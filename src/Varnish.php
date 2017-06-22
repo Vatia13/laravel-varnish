d6f94b8265d45d410edf1c9e54ad4ab2b53c8cba
Salikh Gurgenidze <vatia0@gmail.com>
2017-06-22 22:39:48 +0400

    flush url

--- src/Varnish.php
+++ src/Varnish.php
@@ -18,13 +18,8 @@ class Varnish
 
         $command = $this->generateBanCommand($host,$url);
 
-        if(is_array($command)){
-            foreach($command as $com){
-                $this->executeCommand($com);
-            }
-        }else{
-            return $this->executeCommand($command);
-        }
+        return $this->executeCommand($command);
+
     }
 
     /**
@@ -45,11 +40,11 @@ class Varnish
 
     public function generateBanCommand($hosts,$url = false)
     {
-        if($url == false){
-            if (! is_array($hosts)) {
-                $hosts = [$hosts];
-            }
+        if (! is_array($hosts)) {
+            $hosts = [$hosts];
+        }
 
+        if($url == false){
             $hostsRegex = collect($hosts)
                 ->map(function (string $host) {
                     return "(^{$host}$)";
@@ -59,14 +54,8 @@ class Varnish
 
 
         $config = config('laravel-varnish');
-        if($url == true){
-            $commands = [];
-
-            foreach($hosts as $host){
-                $commands[] = "sudo varnishadm -S {$config['administrative_secret']} -T {$config['administrative_host']}:{$config['administrative_port']} ban 'req.url == {$host}'";
-            }
-
-            return $commands;
+        if($url == true && count($hosts) > 0){
+            return "sudo varnishadm -S {$config['administrative_secret']} -T {$config['administrative_host']}:{$config['administrative_port']} ban 'req.url == {$hosts[0]}'";
         }
         return "sudo varnishadm -S {$config['administrative_secret']} -T {$config['administrative_host']}:{$config['administrative_port']} 'ban req.http.host ~ {$hostsRegex}'";
     }
